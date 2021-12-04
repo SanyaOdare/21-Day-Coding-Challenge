1109ade61ad7a498c91d761d5ef402e516edf651
Sanya Oluwadare <sanyaodare@gmail.com>
2021-12-04 21:56:56 +0100

    add day1 js file

--- day1.js
+++ day1.js
@@ -1,1 +1,10 @@
-
+const parseMessage = (origin, message) => {
+  // Code here!
+    return `${origin}: ${message}`;
+    console.log(parseMessage);
+  // Want to see what's going on inside your function? 
+  // You can use console.log() to debug and print variables to the console below.
+  
+  // Remember to return a value!
+}
+parseMessage("Mission Control", "Hello there!");
